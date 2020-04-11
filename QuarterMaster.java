91692bb5a473361873d95f58c4ccb340ea4b3617
wskel <w20skel@siena.edu>
2020-04-11 13:06:22 -0400

    Added QuarterMaster class for procuring weapons at random.

---
+++ QuarterMaster.java
@@ -0,0 +1,44 @@
+// going to be lazy about imports in this class
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.util.Random;
+
+/**
+ * Procures weapons
+ *
+ * @author Kristi Boardman, Cameron Costello, Will Skelly, Jake Burch
+ * @version Spring 2020
+ */
+public class QuarterMaster
+{
+    public static Weapon getRandomWeapon(JComponent container, Point position){
+
+        Random rand = new Random();
+
+        int num = rand.nextInt(4);
+
+        Weapon toReturn;
+
+        switch(num){
+            case 0:
+            toReturn = new MolotovCocktail(container,position);
+            break;
+            case 1:
+            toReturn = new Grenade(container,position);
+            break;
+            case 2:
+            toReturn = new TNT(container,position);
+            break;
+            case 3:
+            toReturn = new Boulder(container,position);
+            break;
+            default:
+            toReturn = new Boulder(container,position);
+        }
+
+        return toReturn;
+    }
+}
