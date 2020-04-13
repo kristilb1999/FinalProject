88cf947c1ce21c657d2eb77693be04f90e493e80
c03cost <c03cost@siena.edu>
2020-04-13 17:02:23 -0400

    We made the Zombie classes, set their individual strengths, set their hit until death, and set sizes. Strengths and sizes have not been picked yet.

---
+++ Pirate.java
@@ -0,0 +1,58 @@
+// going to be lazy about imports in this class
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.geom.*;
+
+/**
+ * Write a description of class AverageZombie here.
+ *
+ * @author Cameron Costello, Kristi Boardman, Jacob Burch, Will Skelly
+ * @version Spring 2020
+ */
+public class Pirate extends Soldier
+{
+    
+    private static final int SIZE = 50;
+    
+    private static final int STRENGTH = 10;
+
+    /**
+     * Constructor for objects of class AverageZombie
+     */
+    public Pirate(Point2D.Double position, JComponent container)
+    {
+        super(position, container);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        typeFilePath = "soldierTypeFour.png";
+        type = toolkit.getImage(typeFilePath).getScaledInstance(SIZE,SIZE,0);
+        hitsUntilDeath = 4;
+        
+    }
+
+    @Override
+    public void paint(Graphics g) 
+    {
+        if(!done) {
+            g.drawImage(type, (int)position.x, (int)position.y, null);
+        } else {
+
+        }
+    }
+    
+    @Override
+    public int getStrength() 
+    {
+        return STRENGTH;
+    }
+    
+    @Override
+    public int getSize() 
+    {
+        return SIZE;
+    }
+    
+}
+
