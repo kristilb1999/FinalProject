1935f7da1b83abe7cbe89e456ca70a5774a764ad
Cameron <c03cost@siena.edu>
2020-04-19 15:20:41 -0400

    We did not change anything in the inelastic weapon class.

--- InelasticWeapon.java
+++ InelasticWeapon.java
@@ -40,19 +40,7 @@ abstract public class InelasticWeapon extends Weapon
             //EVERY ITERATION, UPDATE THE COORDINATES
             position.x += velocity.x;
             position.y += velocity.y;
-
-            //WHEN THIS PART IS COMMENTED OUT, THE WEAPON WILL FLY PAST THE BORDER
-            //OF THE SCREEN AND COME BACK DOWN TOWARD THE GRAVITY ONCE ITS VELOCITY
-            //BECOMES NEGATIVE -- DO WE LIKE THIS? I LIKE THIS.
             
-            // //BREAK WHEN THE WEAPON REACHES THE CIELING
-            // if (position.y < 0) {
-                // position.y = 0;
-                // done = true;
-                // velocity.x = 0;
-                // velocity.y = 0;
-            // }
-
             //BREAK WHEN THE WEAPON REACHES THE FLOOR
             if (position.y > yMax) {
                 position.y = yMax;
