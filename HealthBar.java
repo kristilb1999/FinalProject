e6e318fc4466ca58aa4c93b0b3905f307303debb
wskel <w20skel@siena.edu>
2020-04-21 03:02:50 -0400

    Made HealthBar thread-safe.

--- HealthBar.java
+++ HealthBar.java
@@ -8,7 +8,10 @@ import javax.swing.event.*;
 import java.awt.geom.*;
 
 /**
- * Write a description of class HealthBar here.
+ * This class is a thread-safe health bar which keeps track of
+ * an amount of health and a JLabel, which it updates the
+ * color and text of as the healthbar gets calls to its
+ * reduce method.
  *
  * @author Kristi Boardman, Cameron Costello, Will Skelly, Jake Burch
  * @version Spring 2020
@@ -18,10 +21,10 @@ public class HealthBar extends Thread
     //THE DELAY TIME FOR THE SLEEP METHOD
     private static final int DELAY_TIME = 33;
 
-    //THE START HEALTH OF THE TOWER
+    //THE START HEALTH
     private static final int START_HEALTH = 30;
 
-    //THE COLORS FOR THE TOWER HEALTH LABEL AND THE DIFFICULTY BUTTONS
+    //THE COLORS FOR THE LABEL
     private static final Color FULL_HEALTH_COLOR = new Color(13, 201, 6);
     private static final Color MED_HEALTH_COLOR = new Color(245, 243, 110);
     private static final Color LOW_HEALTH_COLOR = new Color(176, 41, 32);
@@ -29,28 +32,41 @@ public class HealthBar extends Thread
     private Color med_health_color;
     private Color low_health_color;
 
-    //THE AMOUNT OF HEALTH THE TOWER HAS
-    private int towerHealth;
-    
-    //THE AMOUNT OF HEALTH THE TOWER HAS
-    private int startHealth = START_HEALTH;
+    //THE AMOUNT OF HEALTH
+    private int currentHealth;
 
-    //THE LABEL THAT DISPLAYS THE AMOUNT OF HEALTH THE TOWER HAS LEFT
+    //THE AMOUNT OF HEALTH TO START WITH
+    private int startHealth;
+
+    //THE LABEL THAT DISPLAYS THE AMOUNT OF HEALTH REMAINING
     private JLabel healthBarLabel;
 
+    //IF THE HEALTHBAR IS DONE
     private boolean done;
 
+    //THE TEXT OF THE HEALTHBAR
     private String text;
 
+    //OBJECTS THAT SERVE AS LOCKS FOR THREAD SAFETY
+    private Object currentHealthLock = new Object();
+    private Object startHealthLock = new Object();
+    private Object colorsLock = new Object();
+    private Object textLock = new Object();
+    private Object doneLock = new Object();
+    private Object visibleLock = new Object();
+
     public HealthBar(){
-        this(new JLabel());
+        this("");
     }
-    
+
     public HealthBar(String text){
-        this(new JLabel());
-        this.setText(text);
+        this(new JLabel(text));
     }
-    
+
+    public HealthBar(String text,int startHealth){
+        this(new JLabel(text),startHealth);
+    }
+
     public HealthBar(JLabel healthBarLabel){
         this(healthBarLabel,START_HEALTH);
     }
@@ -58,11 +74,15 @@ public class HealthBar extends Thread
     public HealthBar(JLabel healthBarLabel,int startHealth){
         this(healthBarLabel,startHealth,FULL_HEALTH_COLOR,MED_HEALTH_COLOR,LOW_HEALTH_COLOR);
     }
-    
+
+    public HealthBar(String text,int startHealth,Color full_health_color,Color med_health_color,Color low_health_color){
+        this(new JLabel(text),startHealth,full_health_color,med_health_color,low_health_color);
+    }
+
     public HealthBar(JLabel healthBarLabel,int startHealth,Color full_health_color,Color med_health_color,Color low_health_color){
         this.healthBarLabel = healthBarLabel;
         this.startHealth = startHealth;
-        this.towerHealth = startHealth;
+        this.currentHealth = startHealth;
         this.done = false;
         this.text = healthBarLabel.getText();
         this.full_health_color = full_health_color;
@@ -78,77 +98,153 @@ public class HealthBar extends Thread
                 System.out.print(e);
             }
 
-            //COLOR THE TOWER HEALTH MESSAGE BASED ON AMOUNT OF HEALTH LEFT
-            Color newHealthBarColor;
-            if(towerHealth >= 2 * startHealth / 3) {
-                newHealthBarColor = this.full_health_color;
-            } else if(towerHealth > startHealth / 3) {
-                newHealthBarColor = this.med_health_color;
-            } else {
-                newHealthBarColor = this.low_health_color;
+            synchronized (colorsLock) {
+                //COLOR THE TEXT BASED ON AMOUNT OF HEALTH LEFT
+                Color newHealthBarColor;
+                if(currentHealth >= 2 * startHealth / 3) {
+                    newHealthBarColor = this.full_health_color;
+                } else if(currentHealth > startHealth / 3) {
+                    newHealthBarColor = this.med_health_color;
+                } else {
+                    newHealthBarColor = this.low_health_color;
+                }
+                healthBarLabel.setForeground(newHealthBarColor);
             }
-            healthBarLabel.setForeground(newHealthBarColor);
-            healthBarLabel.setText(this.text + towerHealth);
+
+            synchronized (textLock) {
+                healthBarLabel.setText(this.text + currentHealth);
+            }
         }
     }
 
     public void setColors(Color full_health_color,Color med_health_color,Color low_health_color){
-        this.full_health_color =  full_health_color;
-        this.med_health_color = med_health_color;
-        this.low_health_color = low_health_color;
+        synchronized (colorsLock) {
+            this.full_health_color =  full_health_color;
+            this.med_health_color = med_health_color;
+            this.low_health_color = low_health_color;
+        }
     }
 
-    public void setHealth(int newHealth){
-        this.towerHealth = newHealth;
+    public void setHealth(int health){
+        synchronized (currentHealthLock) {
+            this.currentHealth = health;
+        }
     }
 
     public int getHealth(){
-        return this.towerHealth;
+        synchronized (currentHealthLock) {
+            return this.currentHealth;
+        }
+    }
+
+    public void setStartHealth(int health){
+        synchronized (startHealthLock) {
+            this.startHealth = health;
+        }
+    }
+
+    public int getStartHealth(){
+        synchronized (startHealthLock) {
+            return this.startHealth;
+        }
     }
 
     public void setText(String text){
-        this.text = text;
-        healthBarLabel.setText(this.text);
+        synchronized (textLock) {
+            this.text = text;
+            healthBarLabel.setText(this.text);
+        }
     }
-    
+
     public String getText(){
-        return this.text;
+        synchronized (textLock) {
+            return this.text;
+        }
     }
-    
+
     public void setLabel(JLabel label){
-        this.healthBarLabel = label;
-        this.text = this.healthBarLabel.getText();
+        synchronized (textLock) {
+            this.healthBarLabel = label;
+            this.text = this.healthBarLabel.getText();
+        }
     }
-    
+
     public JLabel getLabel(){
-        return this.healthBarLabel;
+        synchronized (textLock) {
+            return this.healthBarLabel;
+        }
     }
-    
+
     public boolean done(){
-        return this.done;   
+        synchronized (doneLock) {
+            return this.done;   
+        }
     }
 
     public void setDone(boolean isDone){
-        this.done = isDone;   
+        synchronized (doneLock) {
+            this.done = isDone;   
+        }
     }
 
     public void reduce(int damage){
-        this.towerHealth = (this.towerHealth - damage < 0 ? 0 : this.towerHealth - damage);
+        synchronized (currentHealthLock) {
+            //CURRENT HEALTH CAN HAVE A MINUMUM OF ZERO
+            this.currentHealth = (this.currentHealth - damage < 0 ? 0 : this.currentHealth - damage);
+        }
     }
 
     public void setVisible(boolean isVisible){
-        this.healthBarLabel.setVisible(isVisible);
+        synchronized (visibleLock) {
+            this.healthBarLabel.setVisible(isVisible);
+        }
     }
-    
-    public void resetDefaultHealth(){
-        this.towerHealth = START_HEALTH;
+
+    public boolean isVisible(){
+        synchronized (visibleLock) {
+            return healthBarLabel.isVisible();
+        }
     }
-    
+
+    public void resetHealthDefault(){
+        synchronized (currentHealthLock) {
+            this.currentHealth = START_HEALTH;
+        }
+    }
+
     public void resetHealth(){
-        this.towerHealth = this.startHealth;
+        synchronized (currentHealthLock) {
+            this.currentHealth = this.startHealth;
+        }
     }
-    
+
     public void resetHealth(int startHealth){
-        this.towerHealth = startHealth;
+        synchronized (currentHealthLock) {
+            this.currentHealth = startHealth;
+        }
+    }
+
+    public Object getHealthLock(){
+        return currentHealthLock;
+    }
+
+    public Object getStartHealthLock(){
+        return startHealthLock;
+    }
+
+    public Object getColorsLock(){
+        return colorsLock;
+    }
+
+    public Object getTextLock(){
+        return textLock;
+    }
+
+    public Object getDoneLock(){
+        return doneLock;
+    }
+
+    public Object getVisibleLock(){
+        return visibleLock;
     }
 }
