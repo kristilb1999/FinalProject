085fd0d341268d6c85dede716bc7f49efe35b58f
Cameron <c03cost@siena.edu>
2020-04-22 17:57:21 -0400

    Created the second shopper, Cameron, with a morality number of 3.

---
+++ SupermarketShoppers/Cameron.java
@@ -0,0 +1,35 @@
+import java.util.Vector;
+import java.util.Random;
+/**
+ * Write a description of class Shoppers here.
+ *
+ * @author Cameron Costello, Kristi Boardman, Will Skelly, Jacob Burch
+ * @version Spring 2020
+ */
+public class Cameron extends Shopper
+{
+
+    public static final int MORALITY_NUM = 2;
+
+    public static final int MAX_CASH = 600;
+
+    public static final double JAIL_PROB = 20;
+    /**
+     * Constructor for objects of class Shoppers
+     */
+    public Cameron(ShoppingList item)
+    {
+        super(item);
+
+        morality = MORALITY_NUM;
+        cash = random.nextInt(MAX_CASH / MORALITY_NUM) + 1;
+        jailedProb = random.nextInt((int) JAIL_PROB * MORALITY_NUM);
+    }
+
+    @Override
+    public void run()
+    {
+
+    }
+}
+
