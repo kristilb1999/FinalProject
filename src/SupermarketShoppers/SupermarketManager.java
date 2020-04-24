357be70105e31f7dd51e691ab94225c726751a28
JakeBurch <js01burc@siena.edu>
2020-04-24 17:57:56 -0400

    Added SupermarketManager.java which will contain the main method and is where the initial inventory of the store will be decided upon

---
+++ src/SupermarketShoppers/SupermarketManager.java
@@ -0,0 +1,15 @@
+package SupermarketShoppers;
+
+
+/**
+ * Starts the simulation and passes in the data used.
+ *
+ * @author Cameron Costello, Kristi Boardman, Will Skelly, Jacob Burch
+ * @version Spring 2020
+ */
+public class SupermarketManager
+{
+    public static void main(String args[]){
+        
+    }
+}
