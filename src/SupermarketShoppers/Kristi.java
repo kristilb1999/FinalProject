28bd539f21a28daeeea89cdf73aae9054ebf2754
Cameron <c03cost@siena.edu>
2020-04-26 13:38:23 -0400

    Changed the Kristi class to allow it to start stealing as well. Also changed the constructor to take in a supermarket manager reference.

--- src/SupermarketShoppers/Kristi.java
+++ src/SupermarketShoppers/Kristi.java
@@ -10,50 +10,66 @@ import java.util.Random;
  */
 public class Kristi extends Shopper
 {
-    
+
     public static final int MORALITY_NUM = 3;
-    
+
     public static final int MAX_CASH = 1000;
-    
+
     public static final double ONE_HUNDRED = 100;
+    
+    public boolean startedStealing;
     /**
      * Constructor for objects of class Shoppers
      */
-    public Kristi(Vector<Item> shoppingList, Inventory inventory, int number, Jail jail)
+    public Kristi(Vector<Item> shoppingList, Inventory inventory, int number, Jail jail, SupermarketManager supermarket)
     {
-        super(shoppingList, inventory, number, jail);
+        super(shoppingList, inventory, number, jail, supermarket);
 
         morality = MORALITY_NUM;
         cash = random.nextInt(MAX_CASH / MORALITY_NUM) + 1;
         jailedProb = (random.nextDouble() * ONE_HUNDRED) * MORALITY_NUM;
     }
-    
+
     @Override
     public void run()
     {
         System.out.println(toString());
         int i = 0;
+        int listSize = shoppingList.size();
         while(!done) {
             Item currentItem = shoppingList.get(i);
-            
+
             int index = inventory.containsItem(currentItem);
-            
+
             if(index > -1){
-                
+
                 Item itemToCheck = inventory.getList().get(index);
-                
+
                 int itemQuantity = currentItem.getItemQuantity();
                 int numInInventory = itemToCheck.getItemQuantity();
-                
-                if(numInInventory >= itemQuantity){
+
+                if (numInInventory >= itemQuantity && i > listSize)
+                {
+                    startedStealing = true;
                     itemToCheck.updateQuantity(itemQuantity);
                     currentItem.setQuantity(0);
-                    shoppingList.remove(i);
-                }else{
+                    shoppingList.remove(i); 
+                }else if (numInInventory < itemQuantity && i > listSize){
+                    startedStealing = true;
                     itemToCheck.setQuantity(0);
                     currentItem.updateQuantity(numInInventory);
                 }
-                
+                else if(numInInventory >= itemQuantity){
+                    itemToCheck.updateQuantity(itemQuantity);
+                    cash -= itemQuantity * currentItem.getPrice();
+                    currentItem.setQuantity(0);
+                    shoppingList.remove(i);
+                }else{
+                    cash -= numInInventory * currentItem.getPrice();
+                    itemToCheck.setQuantity(0);
+                    currentItem.updateQuantity(numInInventory);
+                }
+
             }else{
                 int numItemsToAdd = random.nextInt(3) + 1;
                 for(int k = 0; k < numItemsToAdd; k++) {
@@ -62,24 +78,29 @@ public class Kristi extends Shopper
                     shoppingList.add(itemToAdd);
                 }  
             }
-            
+
             i++;
-            
+
             done = shoppingList.isEmpty() || i > shoppingList.size();
-            
+
         }
-        
+
         System.out.println(toString());
-       
+
+    }
+
+    public boolean isStealing(boolean stealing)
+    {
+        return startedStealing;
     }
     
     @Override
     public String toString(){
-        
+
         String toPrint = "Kristi's Shopping List:\n";
         toPrint += "Shopper number: " + shopperNumber + "\n";
         toPrint += shoppingList.toString();
         return toPrint;
-        
+
     }
 }
