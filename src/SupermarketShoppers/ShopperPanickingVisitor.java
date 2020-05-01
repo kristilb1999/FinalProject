e4a0d84483e2125c7c7f561147103d198f5694e8
wskel <w20skel@siena.edu>
2020-05-01 15:50:57 -0400

    Added visitor pattern for stealing and panicking checks.

---
+++ src/SupermarketShoppers/ShopperPanickingVisitor.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SupermarketShoppers;
+
+/**
+ *
+ * @author Wil
+ */
+public class ShopperPanickingVisitor implements ShoppingVisitor{
+    
+    @Override
+    public boolean visit(Kristi shopper){
+        
+        
+        return false;
+    }
+    
+    @Override
+    public boolean visit(Jacob shopper){
+        
+        
+       return false;
+    }
+    
+    @Override
+    public boolean visit(Shopper shopper){
+        
+        
+        return false;
+    }
+
+    @Override
+    public boolean visit(Will shopper) {
+        return shopper.isPanicking();
+    }
+
+    @Override
+    public boolean visit(Cameron shopper) {
+        return shopper.isPanicking();
+    }
+    
+}
---
+++ src/SupermarketShoppers/ShopperStealingVisitor.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SupermarketShoppers;
+
+/**
+ *
+ * @author Wil
+ */
+public class ShopperStealingVisitor implements ShoppingVisitor{
+    
+    @Override
+    public boolean visit(Kristi shopper){
+        
+        
+        return shopper.isStealing();
+    }
+    
+    @Override
+    public boolean visit(Jacob shopper){
+        
+        
+       return shopper.isStealing();
+    }
+    
+    @Override
+    public boolean visit(Shopper shopper){
+        
+        
+        return false;
+    }
+
+    @Override
+    public boolean visit(Will shopper) {
+        return false;
+    }
+
+    @Override
+    public boolean visit(Cameron shopper) {
+        return false;
+    }
+    
+    
+    
+}
